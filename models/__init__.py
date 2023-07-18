4647be8538f57ff4c6e65502979ed773a2b4602c
wideplain <44184669+Evil-Wideplain@users.noreply.github.com>
2023-07-18 15:41:50 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.attention import *
-from models.backbones import *
-from models.backbones2 import *
-from models.Encoder import *
-from models.frameworks import *
-from models.MMB import *
-from models.TC import *
